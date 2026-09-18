9eca1b2d6b5a1b2dbb8e9a02c604abced1a6c31e
ev-kai <evkairas@gmail.com>
2026-09-18 22:59:32 +0300

    lists,loops,modules

---
+++ day04/lists.py
@@ -0,0 +1,42 @@
+
+import smtplib
+
+
+
+# soc_members = ["Giorgakis", "Spyros", "Mariakr", "Foustanas", "Vaggelakis","Varsamas"]
+
+# print(soc_members[1]) 
+ 
+ 
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user="vaggeliskairas@gmail.com", password="hzxepvwycrkaeltd") 
+# send_email = connection.sendmail(
+#     from_addr="vaggeliskairas@gmail.com", 
+#     to_addrs=["varsamas.john@gmail.com", "vaggeliskairas@gmail.com"],
+#     msg=f"Subject: Atoma gia Spit Upno\n{soc_members[1]} kai {soc_members[2]} einai ta atoma pou tha paei na koimithoun mazi stin Spiti."
+# )
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day05/loops.py
@@ -0,0 +1,30 @@
+
+
+
+
+# student_scores = [85, 92, 78, 96, 88]
+
+
+
+# max_score = 0
+
+
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+
+
+
+# print(f"Highest score: {max_score}")
+
+
+
+total = 0
+for number in range(1, 101):
+    
+
+   total += number
+   
+print(total)
+    
+
